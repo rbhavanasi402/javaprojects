e0893a61264cc5a27fe087fe4fdf92ca92f09581
raghu.bhavanasi <raghu.bhavanasi@BDC7-L-G8YRPP3.dir.svc.accenture.com>
2022-12-14 13:04:49 +0530

    Updated

--- spring_demo/src/com/spring/demo/SpringDemoApplication.java
+++ spring_demo/src/com/spring/demo/SpringDemoApplication.java
@@ -3,6 +3,6 @@ package com.spring.demo;
 public class SpringDemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Testing Java Project..");
+		System.out.println("Testing Java Project....");
 	}
 }
